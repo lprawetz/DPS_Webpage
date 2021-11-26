2993fea17fd33beb93c07edd01c77cedcf370164
Lukas Prawetz <lprawetz@mail.de>
2021-11-26 19:12:57 +0100

    footer visuals

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render("error", { err });
 });
 
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server is running in Port ${port}!`)
 });
